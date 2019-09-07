ccfae44be3bc6e3fa3e16bd14f7fa490275aa612
unknown <tcutlip08@gmail.com>
2019-09-07 09:58:14 -0400

    fixed handlebars syntax to show devour button

---
+++ public/assets/js/run.js
@@ -0,0 +1,45 @@
+$(function () {
+    $(".devour").on("click", event => {
+        const id = $(event.currentTarget).data("id");
+        console.log(id);
+        console.log("fuck");
+        // const newDevour = $(event.currentTarget).data("newdevour");
+        // const newDevourState = {
+        //     devoured: newDevour
+        // }
+        // $.ajax("/api/burgers/" + id, {
+        //     type: "PUT",
+        //     data: newDevourState
+        // }).then(() => {
+        //     console.log("changed devoured to", newDevour);
+        //     location.reload();
+        // })
+    });
+    $('.create-form').on('submit', event => {
+        // event.preventDefault();
+        // const newBurger = {
+        //     burger_name: $("#burger").val().trim(),
+        //     devoured: 1
+        // };
+        // $.ajax("/api/burgers", {
+        //     type: 'POST',
+        //     data: newBurger
+        // }).then(() => {
+        //     console.log("new burger created");
+        //     location.reload();
+        // })
+    });
+    // $(".delete-burger").on("click", function (event) {
+    //     var id = $(this).data("id");
+    //     // Send the DELETE request.
+    //     $.ajax("/api/burgers/" + id, {
+    //         type: "DELETE"
+    //     }).then(
+    //         function () {
+    //             console.log("deleted burger", id);
+    //             // Reload the page to get the updated list
+    //             location.reload();
+    //         }
+    //     );
+    // });
+});
